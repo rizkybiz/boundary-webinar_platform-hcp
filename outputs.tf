4dae7069acbbc72648a9d5083318ad953f500c9c
JoeStack <joern@hashicorp.com>
2023-08-04 14:07:15 +0200

    HCP Cluster Service Selection 2.5

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "boundary_cluster_url" {
-  value = hcp_boundary_cluster.example.*.cluster_url
+  value = hcp_boundary_cluster.example[0].cluster_url
 }
 
 output "vault_cluster_public_url" {
-  value = hcp_vault_cluster.example.*.vault_public_endpoint_url
+  value = hcp_vault_cluster.example[0].vault_public_endpoint_url
 }
 
 output "vault_cluster_private_url" {
-  value = hcp_vault_cluster.example.*.vault_private_endpoint_url
+  value = hcp_vault_cluster.example[0].vault_private_endpoint_url
 }
 
 output "vault_namespace" {
-  value = hcp_vault_cluster.example.*.namespace
+  value = hcp_vault_cluster.example[0].namespace
 }
 
 output "vault_admin_token" {
-  value = hcp_vault_cluster_admin_token.example.*.token
+  value = hcp_vault_cluster_admin_token.example[0].token
   sensitive = true
 }
 
